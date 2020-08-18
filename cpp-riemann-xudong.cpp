1e59e7e981b85719ee295753daa6397af1079a08
Xudong Zhuang <xudong_zhuang@berkeley.edu>
2020-08-19 00:51:24 +0800

    add source code of hw6 by xudong

---
+++ cpp-riemann-xudong.cpp
@@ -0,0 +1,32 @@
+#include <iostream> // for std::cout, std::endl
+
+double calculate_y(double x)
+{
+    return 1.0 / (x * x + 1.0);
+}
+
+double calculate_integral(double a, double b)
+{
+    // set the number of point
+    int n_points = 95;
+    double dx = (b - a) / n_points;
+    double integral = 0.0;
+    double x, y;
+    for(x = a + dx / 2.0; x < b ; x += dx)
+    {
+        y = calculate_y(x);
+        integral += y * dx;
+    }
+    return integral;
+}
+
+int main(void)
+{
+    // set initial parameters
+    double a = 0.0, b = 1.0;
+    double observed_integral;
+    observed_integral = calculate_integral(a, b);
+    std::cout << "The integral of 1/(x^2 + 1) from " << a << " to " << b
+            << " is " << observed_integral << "." << std::endl;
+    return 0;
+}
